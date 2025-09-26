97c65a7a3520a1bb822f43ab84c22a4169f8d599
Álvaro Castresana <alvaro@laheroica.com.ar>
2025-09-26 02:20:41 -0300

    fix: add @vitejs/plugin-react and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'  // 👈 usar así si va directo en athlon.com.ar
-  // base: '/fitfest/'  // 👈 usar así si lo vas a poner en athlon.com.ar/fitfest/
 })
